d77da0d4495e83db2f50128a4cbeda6a6a8a15bd
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2025-10-13 16:56:15 +0900

    move the code refering the package `"flag"` directly from `main.go` to `flags.go`

---
+++ startup/flags.go
@@ -0,0 +1,47 @@
+package startup
+
+import (
+	"flag"
+
+	"github.com/hymkor/struct2flag"
+)
+
+type Flag struct {
+	CellWidth     string `flag:"w,set the \x60widths\x60 of cells like '-w DefaultWidth,COL0:WIDTH0,COL1:WIDTH1,...'. COLn is the index starting from 0"`
+	Header        uint   `flag:"h,the number of row-header"`
+	Tsv           bool   `flag:"t,use TAB as field-separator"`
+	Csv           bool   `flag:"c,use Comma as field-separator"`
+	Semicolon     bool   `flag:"semicolon,use Semicolon as field-separator"`
+	Iana          string `flag:"iana,IANA-registered-name to decode/encode NonUTF8 text(for example: Shift_JIS,EUC-JP... )"`
+	NonUTF8       bool   `flag:"nonutf8,do not judge as utf8"`
+	Help          bool   `flag:"help,this help"`
+	Auto          string `flag:"auto,autopilot"`
+	Utf16le       bool   `flag:"16le,Force read/write as UTF-16LE"`
+	Utf16be       bool   `flag:"16be,Force read/write as UTF-16BE"`
+	FixColumn     bool   `flag:"fixcol,Do not insert/delete a column"`
+	ReadOnly      bool   `flag:"readonly,Read Only Mode"`
+	ProtectHeader bool   `flag:"p,Protect the header line"`
+	Title         string `flag:"title,Set title string"`
+	ReverseVideo  bool   `flag:"rv,Enable reverse-video display (invert foreground and background colors)"`
+	DebugBell     bool   `flag:"debug-bell,Enable Debug Bell"`
+	flagSet       *flag.FlagSet
+}
+
+func NewFlag() *Flag {
+	return &Flag{
+		CellWidth: "14",
+		Header:    1,
+	}
+}
+
+func (this *Flag) Bind(fs *flag.FlagSet) *Flag {
+	this.flagSet = fs
+	struct2flag.Bind(fs, this)
+	return this
+}
+
+func Run() error {
+	f := NewFlag().Bind(flag.CommandLine)
+	flag.Parse()
+	return f.Run()
+}
--- startup/main.go
+++ startup/main.go
@@ -1,7 +1,6 @@
 package startup
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -10,50 +9,15 @@ import (
 	"github.com/mattn/go-colorable"
 	"github.com/mattn/go-isatty"
 
-	"github.com/hymkor/struct2flag"
-
 	"github.com/hymkor/csvi"
-
 	"github.com/hymkor/csvi/uncsv"
 
 	"github.com/hymkor/csvi/internal/ansi"
 )
 
-type Flag struct {
-	CellWidth     string `flag:"w,set the \x60widths\x60 of cells like '-w DefaultWidth,COL0:WIDTH0,COL1:WIDTH1,...'. COLn is the index starting from 0"`
-	Header        uint   `flag:"h,the number of row-header"`
-	Tsv           bool   `flag:"t,use TAB as field-separator"`
-	Csv           bool   `flag:"c,use Comma as field-separator"`
-	Semicolon     bool   `flag:"semicolon,use Semicolon as field-separator"`
-	Iana          string `flag:"iana,IANA-registered-name to decode/encode NonUTF8 text(for example: Shift_JIS,EUC-JP... )"`
-	NonUTF8       bool   `flag:"nonutf8,do not judge as utf8"`
-	Help          bool   `flag:"help,this help"`
-	Auto          string `flag:"auto,autopilot"`
-	Utf16le       bool   `flag:"16le,Force read/write as UTF-16LE"`
-	Utf16be       bool   `flag:"16be,Force read/write as UTF-16BE"`
-	FixColumn     bool   `flag:"fixcol,Do not insert/delete a column"`
-	ReadOnly      bool   `flag:"readonly,Read Only Mode"`
-	ProtectHeader bool   `flag:"p,Protect the header line"`
-	Title         string `flag:"title,Set title string"`
-	ReverseVideo  bool   `flag:"rv,Enable reverse-video display (invert foreground and background colors)"`
-	DebugBell     bool   `flag:"debug-bell,Enable Debug Bell"`
-}
-
-func NewFlag() *Flag {
-	return &Flag{
-		CellWidth: "14",
-		Header:    1,
-	}
-}
-
-func (this *Flag) Bind(fs *flag.FlagSet) *Flag {
-	struct2flag.Bind(fs, this)
-	return this
-}
-
 func (f *Flag) Run() error {
 	if f.Help {
-		flag.Usage()
+		f.flagSet.Usage()
 		return nil
 	}
 	disable := colorable.EnableColorsStdout(nil)
@@ -92,17 +56,17 @@ func (f *Flag) Run() error {
 
 	var out io.Writer
 	var reader io.Reader
-	if len(flag.Args()) <= 0 {
+	if len(f.flagSet.Args()) <= 0 {
 		out = colorable.NewColorableStderr()
 	} else {
 		out = colorable.NewColorableStdout()
 	}
-	if len(flag.Args()) <= 0 && isatty.IsTerminal(uintptr(os.Stdin.Fd())) {
+	if len(f.flagSet.Args()) <= 0 && isatty.IsTerminal(uintptr(os.Stdin.Fd())) {
 		// Start with one empty line
 		mode.Comma = '\t'
 	} else {
 		mode.Comma = ','
-		args := flag.Args()
+		args := f.flagSet.Args()
 		if len(args) >= 1 && !strings.HasSuffix(strings.ToLower(args[0]), ".csv") {
 			mode.Comma = '\t'
 		}
@@ -140,10 +104,4 @@ func (f *Flag) Run() error {
 	}.Edit(reader, out)
 
 	return err
-}
-
-func Run() error {
-	f := NewFlag().Bind(flag.CommandLine)
-	flag.Parse()
-	return f.Run()
 }
