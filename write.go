2f877ea0829770f2954d41fbf52c4c9594c92cbe
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-01-18 13:18:22 +0900

    Fix compile error caused with merging

--- write.go
+++ write.go
@@ -97,16 +97,16 @@ func (app *Application) cmdSave() (string, error) {
 				default:
 					row, err := app.fetchFunc()
 					if err != nil && !errors.Is(err, io.EOF) {
-						app.fetch = nil
-						app.tryFetch = nil
+						app.fetchFunc = nil
+						app.tryFetchFunc = nil
 						return
 					}
 					if row != nil {
 						app.push(row)
 					}
 					if errors.Is(err, io.EOF) {
-						app.fetch = nil
-						app.tryFetch = nil
+						app.fetchFunc = nil
+						app.tryFetchFunc = nil
 						return
 					}
 				}
