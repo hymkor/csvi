1dbb268b16074758f6db6b89eb640a3211d8558b
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-03-29 00:30:24 +0900

    Improve internal/nonblock with cancellation and goroutine synchronization
    Import changes of https://github.com/hymkor/bine/pull/69

--- internal/nonblock/nonblock.go
+++ internal/nonblock/nonblock.go
@@ -1,10 +1,20 @@
 package nonblock
 
 import (
+	"context"
 	"errors"
 	"io"
 	"os"
+	"sync"
 	"time"
+)
+
+var (
+	ErrNoKeyResponse = errors.New("no key response")
+
+	// ErrNoDataResponse indicates that no more data will be produced.
+	// Currently it is aliased to io.EOF for backward compatibility.
+	ErrNoDataResponse = io.EOF // errors.New("no data response")
 )
 
 type keyResponse struct {
@@ -21,43 +31,58 @@ type NonBlock[T any] struct {
 	chKeyReq   chan struct{}
 	chKeyRes   chan keyResponse
 	chDataRes  chan dataResponse[T]
-	chStopReq  chan struct{}
+	cancel     context.CancelFunc
+	wg         sync.WaitGroup
 	noMoreData bool
 }
 
 func New[T any](keyGetter func() (string, error),
 	dataGetter func() (T, error)) *NonBlock[T] {
 
+	ctx, cancel := context.WithCancel(context.Background())
+
 	w := &NonBlock[T]{
 		chKeyReq:  make(chan struct{}),
 		chKeyRes:  make(chan keyResponse),
 		chDataRes: make(chan dataResponse[T]),
-		chStopReq: make(chan struct{}),
+		cancel:    cancel,
 	}
+	w.wg.Add(2)
 
 	go func() {
-		for range w.chKeyReq {
-			key, err := keyGetter()
-			w.chKeyRes <- keyResponse{key: key, err: err}
+		defer w.wg.Done()
+		defer close(w.chKeyRes)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-w.chKeyReq:
+				key, err := keyGetter()
+				select {
+				case w.chKeyRes <- keyResponse{key: key, err: err}:
+				case <-ctx.Done():
+					return
+				}
+
+			}
 		}
 	}()
 
 	go func() {
+		defer w.wg.Done()
+		defer close(w.chDataRes)
 		for {
-			select {
-			case <-w.chStopReq:
+			if dataGetter == nil {
 				return
-			default:
-				if dataGetter == nil {
-					close(w.chDataRes)
-					return
-				}
-				data, err := dataGetter()
-				w.chDataRes <- dataResponse[T]{val: data, err: err}
-				if errors.Is(err, io.EOF) {
-					close(w.chDataRes)
-					return
-				}
+			}
+			data, err := dataGetter()
+			select {
+			case w.chDataRes <- dataResponse[T]{val: data, err: err}:
+			case <-ctx.Done():
+				return
+			}
+			if errors.Is(err, io.EOF) {
+				return
 			}
 		}
 	}()
@@ -69,21 +94,25 @@ func (w *NonBlock[T]) GetOr(work func(val T, err error) bool) (string, error) {
 	w.chKeyReq <- struct{}{}
 	if w.noMoreData {
 		res, ok := <-w.chKeyRes
-		if ok {
-			return res.key, res.err
+		if !ok {
+			return "", ErrNoKeyResponse
 		}
-		return "", io.EOF
+		return res.key, res.err
 	}
 	for {
 		select {
 		case res, ok := <-w.chKeyRes:
-			if ok {
-				return res.key, res.err
+			if !ok {
+				return "", ErrNoKeyResponse
 			}
+			return res.key, res.err
 		case res, ok := <-w.chDataRes:
 			if !ok || work == nil || !work(res.val, res.err) {
-				res := <-w.chKeyRes
+				res, ok := <-w.chKeyRes
 				w.noMoreData = true
+				if !ok {
+					return "", ErrNoKeyResponse
+				}
 				return res.key, res.err
 			}
 		}
@@ -95,7 +124,7 @@ func (w *NonBlock[T]) Fetch() (T, error) {
 	if !ok {
 		w.noMoreData = true
 		var zero T
-		return zero, io.EOF
+		return zero, ErrNoDataResponse
 	}
 	if errors.Is(res.err, io.EOF) {
 		w.noMoreData = true
@@ -112,20 +141,20 @@ func (w *NonBlock[T]) TryFetch(timeout time.Duration) (T, error) {
 	var zero T
 	select {
 	case res, ok := <-w.chDataRes:
-		if ok {
-			if errors.Is(res.err, io.EOF) {
-				w.noMoreData = true
-			}
-			return res.val, res.err
+		if !ok {
+			w.noMoreData = true
+			return zero, ErrNoDataResponse
 		}
-		w.noMoreData = true
-		return zero, io.EOF
+		if errors.Is(res.err, io.EOF) {
+			w.noMoreData = true
+		}
+		return res.val, res.err
 	case <-time.After(timeout):
 		return zero, os.ErrDeadlineExceeded
 	}
 }
 
 func (w *NonBlock[T]) Close() {
-	close(w.chStopReq)
-	close(w.chKeyReq)
+	w.cancel()
+	w.wg.Wait()
 }
